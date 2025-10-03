86a471985523fd20ae08e0f61405222dfbf8e7e9
‪Mohammad  Gamil <mohammad.metwally.gamil@gmail.com>
2025-10-03 03:19:06 +0300

    feat(ApplicationUser): implement Application user and SocialMdeiaDb

---
+++ SocialMedia.Infrastructure/Persistence/SocialMediaDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SocialMedia.Domain.Entities;
+
+namespace SocialMedia.Infrastructure.Persistence
+{
+    public class SocialMediaDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public SocialMediaDbContext(DbContextOptions<SocialMediaDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- SocialMedia.Web/Program.cs
+++ SocialMedia.Web/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace SocialMedia.Web
 {
     public class Program
@@ -6,6 +9,14 @@ namespace SocialMedia.Web
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // DB context configuratuib
+            builder.Services.AddDbContext<SocialMedia.Infrastructure.Persistence.SocialMediaDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddIdentity<SocialMedia.Domain.Entities.ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<SocialMedia.Infrastructure.Persistence.SocialMediaDbContext>();
+           
+            
             // Add services to the container.
             builder.Services.AddControllersWithViews();
 
@@ -15,13 +26,15 @@ namespace SocialMedia.Web
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+               
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseRouting();
 
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.MapStaticAssets();
